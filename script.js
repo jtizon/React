e4d026697ef013f9cd3bb40d08a621774b0ea78d
Jessie Tizon <jct2418@gmail.com>
2019-05-08 10:10:40 -0600

    Final Commit

---
+++ script.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div>
+            <h1>Hello, everyone!</h1>
+            <p>We think,</p>
+            <ul>
+                <li>React</li>
+                <li>is</li>
+                <li>brilliant!</li>
+            </ul>
+        </div>
+      );
+    }
+
+export default App;
